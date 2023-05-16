0601399c407e1a5b5e44a1121669f3093e22b051
Bfay24 <133755622+Bfay24@users.noreply.github.com>
2023-05-16 16:01:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['265996380416', 'sandaru', true],
+  ['265996380416, 'Bfay', true], 
   [''] 
 ] //Number of owners
 
